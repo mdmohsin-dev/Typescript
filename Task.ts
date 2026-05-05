f682c1d92abe3ed9406cfa0b842fac371f53c4bd
md mohosin <mdmohosin.web@gmail.com>
2026-05-05 16:43:23 +0600

    add union and intersection types for Book and Magazine

--- Task.ts
+++ Task.ts
@@ -1,40 +1,77 @@
-// Task-1
-const user = (name: string, age: number, role?: 'admin' | 'user' | 'guest') => {
-    //     console.log(`
-    //  User Info
-    // ------------------------
-    // Name : ${name}
-    // Age  : ${age}
-    // Role : ${role ?? 'Not Provided'}
-    // `);
+{
+    // Task-1
+    const user = (name: string, age: number, role?: 'admin' | 'user' | 'guest') => {
+        //     console.log(`
+        //  User Info
+        // ------------------------
+        // Name : ${name}
+        // Age  : ${age}
+        // Role : ${role ?? 'Not Provided'}
+        // `);
+    }
+    user('Mohsin', 22, 'admin');
+
+
+
+
+    // TASK-3
+    type Person = {
+        name: string;
+        address: string,
+        hairColor: string;
+        income: number,
+        expense: number,
+        familyMembers: number,
+        skills: string[],
+        maritalStatus: boolean;
+    }
+
+    const person: Person = {
+        name: 'Mohsin',
+        address: 'Kishoreganj',
+        hairColor: 'black',
+        income: 0,
+        expense: 1000,
+        familyMembers: 4,
+        skills: ['Javascript', 'Typescirpt', 'React', 'Mongodb'],
+        maritalStatus: true
+    }
+    console.log(person)
+
+
+
+    // Task-4
+    interface Book {
+        title: string;
+        author: string,
+        pages: number
+    }
+    interface Magazine {
+        title: string;
+        issueNumber: number;
+        publisher: string;
+    }
+
+    type BookOrMagazine = Book | Magazine
+
+    type BookAndMagazine = Book & Magazine
+
+
+    const person1: BookOrMagazine = {
+        title: 'Ts guide',
+        author: "Mohsin",
+        pages: 120
+    }
+
+    const person2: BookAndMagazine = {
+        title: 'Hybrid Content',
+        author: 'Mohsin',
+        pages: 200,
+        issueNumber: 10,
+        publisher: 'ABC'
+    }
+
+    console.log(person1)
+    console.log(person2)
+
 }
-user('Mohsin', 22, 'admin');
-
-
-
-
-// TASK-3
-type Person = {
-    name: string;
-    address: string,
-    hairColor: string;
-    income: number,
-    expense: number,
-    familyMembers: number,
-    skills: string[],
-    maritalStatus: boolean;
-}
-
-const person: Person = {
-    name: 'Mohsin',
-    address: 'Kishoreganj',
-    hairColor: 'black',
-    income: 0,
-    expense: 1000,
-    familyMembers: 4,
-    skills: ['Javascript', 'Typescirpt', 'React', 'Mongodb'],
-    maritalStatus: true
-}
-console.log(person)
-
-
