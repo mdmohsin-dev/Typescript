08c579ffc9a2feb0e1dffd0787dab375d556283e
md mohosin <mdmohosin.web@gmail.com>
2026-05-05 16:36:48 +0600

    feat: define Person type alias with structured properties

--- Task.ts
+++ Task.ts
@@ -1,12 +1,40 @@
 // Task-1
 const user = (name: string, age: number, role?: 'admin' | 'user' | 'guest') => {
-    console.log(`
- User Info
-------------------------
-Name : ${name}
-Age  : ${age}
-Role : ${role ?? 'Not Provided'}
-`);
+    //     console.log(`
+    //  User Info
+    // ------------------------
+    // Name : ${name}
+    // Age  : ${age}
+    // Role : ${role ?? 'Not Provided'}
+    // `);
+}
+user('Mohsin', 22, 'admin');
+
+
+
+
+// TASK-3
+type Person = {
+    name: string;
+    address: string,
+    hairColor: string;
+    income: number,
+    expense: number,
+    familyMembers: number,
+    skills: string[],
+    maritalStatus: boolean;
 }
 
-user('Mohsin', 22, 'admin');
+const person: Person = {
+    name: 'Mohsin',
+    address: 'Kishoreganj',
+    hairColor: 'black',
+    income: 0,
+    expense: 1000,
+    familyMembers: 4,
+    skills: ['Javascript', 'Typescirpt', 'React', 'Mongodb'],
+    maritalStatus: true
+}
+console.log(person)
+
+
