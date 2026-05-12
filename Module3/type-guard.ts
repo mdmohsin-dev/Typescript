dc3d7bb73ae9352a35f72b91995a9a175ef8e622
md mohosin <mdmohosin.web@gmail.com>
2026-05-12 23:41:13 +0600

    Learn instanceof type guard in TypeScript

--- Module3/type-guard.ts
+++ Module3/type-guard.ts
@@ -25,7 +25,7 @@
     }
 
 
-    const getUser = (user: NormalUser | AdminUser)=>{
+    const getUser = (user: NormalUser | AdminUser) => {
         if ('role' in user) {
             console.log(`My name is ${user.name} and my role is ${user.role}`)
         }
@@ -41,7 +41,55 @@
         name: "Admin vai",
         role: 'admin'
     }
-
     getUser(adminUser)
+
+
+
+
+
+
+
+
+    class Animal {
+        name: string;
+        species: string
+
+        constructor(name: string, species: string) {
+            this.name = name;
+            this.species = species;
+        }
+
+        makeSound() {
+            console.log(`This ${this.name}`)
+        }
+    }
+
+    class Dog extends Animal {
+        constructor(name: string, species: string) {
+            super(name, species)
+        }
+        makeSound() {
+            console.log('Dog bark')
+        }
+        makeBark() {
+            console.log('Dog is barking')
+        }
+    }
+
+
+    const isDog = (animal: Animal): animal is Dog => {
+        return animal instanceof Dog
+    }
+
+
+    const getAnimal = (animal: Animal) => {
+        if (isDog(animal)) {
+            animal.makeBark()
+            // animal.
+        }
+    }
+
+
+    const dog = new Dog('Mr.Dog', 'Bark')
 
 }
